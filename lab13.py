953d6a7aba38863afaf0e5e4e292e722a4a25b63
Mohammed.I.Alharbi <name@gmail.com>
2022-10-18 17:27:16 +0300

    lab 13 has been solved :)

---
+++ lab13.py
@@ -0,0 +1,20 @@
+stop_the_app = input("Right 'exit' if you want to exit the app: ")
+
+while stop_the_app != "exit":
+    user_answer = input("do you want to add a new To-Do item? answer by ""y"" for yes and ""n"" for no. ")
+    if user_answer == "y":
+        file = open("to_do.txt", "a", encoding="utf-8")
+        user_notes = input("Please write your note: ")
+        file.write(user_notes)
+        file.close
+    else:
+        user_list = input("do you want to list your To-Do items ? answer 'y' for yes and 'n for no. ")
+        if user_list == "y":
+            print("reading one line:")
+            file = open("to_do.txt", "a+", encoding="utf-8")
+            file.seek(0)
+            print(file.readlines())
+
+            file.close()
+        else:
+            continue
