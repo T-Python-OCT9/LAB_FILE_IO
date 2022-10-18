6ad658f0c8956612894bf40ed434a32148aafbee
Razan Alshehri <alshehri.razan8@gmail.com>
2022-10-18 20:39:06 +0300

    ToDolist

---
+++ toDolist.py
@@ -0,0 +1,11 @@
+x = input("do you want to add a new To-Do item? answer by 'y' for yes and 'n' for no.")
+
+
+if x == "y" :
+  Yes = print = input ("please type in your new To-Do item :")
+elif x == "n":
+  No =print = input ("do you want to list your To-Do items ? answer 'y' for yes and 'n' for no.")
+if No == "y":
+      print ("m")
+else :
+  print ("Yoy should answer by 'y' OR 'n' only ..")
