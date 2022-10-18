62591b970e0922462b13b53d62e58624b6d7881b
Abdullah <a.fahad9@outlook.com>
2022-10-18 19:34:33 +0300

    Answered with While loop this time :D !

--- to_do_list.py
+++ to_do_list.py
@@ -9,10 +9,8 @@
 - Then return again to ther first question and ask again, you coninue this untill the user types in "exit" , then you exit the program. and print to the user "thank you for using the To-Do program, come back again soon"
 
 '''
-
-
-def to_do_list(user_input: str)->str:
-    
+while(True):
+    user_input = input("Do you want to add a new To-Do item? Use y for Yes n for No and Exit to exit the program ! ")
     if user_input == "y":
         user_new_item = input("Type in the new To-Do item : ")
         file = open("to_do.txt","a+", encoding=("utf-8"))
@@ -24,15 +22,10 @@ def to_do_list(user_input: str)->str:
         if user_ask_no == "y":
             file = open("to_do.txt","r", encoding=("utf-8"))
             print(file.readlines())
-            file.close
+            file.close()
+            continue
         else:
-            print("Type exit to exit the program or ", to_do_list())
+            print("Type exit to exit the program or ")
     elif user_input == "exit":
-        print("thank you for using the To-Do program, come back again soon")   
-    else:
-        print("Wrong entry! Type y for Yes and n for No, and exit to close the program ")
-        return to_do_list()
-
-user_input = input("Do you want to add a new To-Do item? ")
-to_do_list(user_input)
-
+        print("thank you for using the To-Do program, come back again soon")
+        break
