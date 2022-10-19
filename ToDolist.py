6fd326e16fcaaa211494733973d5bee88554efd2
Samaa <Samaanawaf@gmail.com>
2022-10-19 13:00:06 +0300

    modified the lab

--- ToDolist.py
+++ ToDolist.py
@@ -17,29 +17,3 @@ while True:
     elif User_Input == "exit":
         print("We're happy to help you , see you next time!") 
         break
-
-'''
-while True:
-    user_input = input("Welcome to Todo list app !, to countinue answer \"y\" if you want to exit the app write \"exit\" :\n ")
-    if user_input == "y":
-        user_new_item = input("what To-Do note do you want to add?: \n ")
-        file = open("TodoList.txt","a", encoding=("utf-8"))
-        file.write(user_new_item  + "\n")
-       
-        file.close
-    elif user_input == "n":
-        user_ask_no = input("do you want to list your To-Do list ?\n")
-        if user_ask_no == "y":
-            file = open("TodoList.txt","r", encoding=("utf-8"))
-            print(file.readlines())
-            file.close()
-            content = file.readlines()
-            
-            for index, notes in enumerate(content):
-                print(f"{index} - {otes}")
-            
-    elif user_input == "exit":
-        print("tWe're happy to help you , see you next time!")
-        break      
-       
-'''
